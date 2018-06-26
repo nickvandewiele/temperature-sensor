9befc06915680690025e2d1a017d69195e5401dd
Nick Vandewiele <nickvandewiele@gmail.com>
2018-06-26 15:12:18 -0400

    add RequestFormatter to collector

--- app/services/collector/project/__init__.py
+++ app/services/collector/project/__init__.py
@@ -1,7 +1,20 @@
 import logging
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask.logging import default_handler
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        record.url = request.url
+        record.remote_addr = request.remote_addr
+        return super().format(record)
+
+formatter = RequestFormatter(
+    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+    '%(levelname)s in %(module)s: %(message)s'
+)
+default_handler.setFormatter(formatter)
 
 
 # instantiate db
